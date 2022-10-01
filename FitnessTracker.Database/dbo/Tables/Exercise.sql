8a711eda892feb2d9e5fb00b8226b461fe2b0721
Nurul Amin <56696105+NurulAmin7990@users.noreply.github.com>
2022-10-01 16:11:52 +0100

    System-Release-011022 (#7)
    * Update README.md (#1) (#2)
    
    Co-authored-by: Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
    
    * Update README.md
    
    * Create superlinter.yml
    
    * Update superlinter.yml
    
    * Update superlinter.yml
    
    * 4 change demo site (#5)
    
    * Updated git ignore file.
    Updated demo link.
    
    * fixed lint errors.
    
    * Fixed lint errors with first headin.
    
    * Initial scaffolding (#6)
    
    Co-authored-by: Kelvin789 <67801802+Kelvin789@users.noreply.github.com>

---
+++ FitnessTracker.Database/dbo/Tables/Exercise.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Exercise] (
+    [ID]           INT            IDENTITY (1, 1) NOT NULL,
+    [DateCreated]  DATETIME2 (7)  NOT NULL,
+    [ExerciseName] NVARCHAR (255) NOT NULL,
+    [IsActive]     BIT            DEFAULT ((1)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ FitnessTracker.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace FitnessTracker.Shared
+{
+    public class Class1
+    {
+
+    }
+}
