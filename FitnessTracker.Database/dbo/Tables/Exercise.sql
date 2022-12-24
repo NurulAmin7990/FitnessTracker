d24afad4a0f44ff6411b27e0b8dd451f7439951a
Nurul Amin <NurulAmin7990@gmail.com>
2022-12-24 14:08:48 +0000

    Remove ignored files

--- FitnessTracker.Database/dbo/Tables/Exercise.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Exercise] (
-    [ID]           INT            IDENTITY (1, 1) NOT NULL,
-    [DateCreated]  DATETIME2 (7)  NOT NULL,
-    [ExerciseName] NVARCHAR (255) NOT NULL,
-    [IsActive]     BIT            DEFAULT ((1)) NOT NULL,
-    PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
