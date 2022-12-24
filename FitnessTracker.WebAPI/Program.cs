9b26f71b6b41c6981df535ceb85cc01044823fb9
Nurul Amin <NurulAmin7990@gmail.com>
2022-12-24 13:57:14 +0000

    Updated web api program configuration

--- FitnessTracker.WebAPI/Program.cs
+++ FitnessTracker.WebAPI/Program.cs
@@ -2,36 +2,42 @@ using FitnessTracker.BusinessLogic.Data;
 using FitnessTracker.BusinessLogic.Interfaces;
 using FitnessTracker.BusinessLogic.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Fitness Tracker API",
+        Description = "An ASP.NET Core Web API for fitness tracking and workout management",
+        TermsOfService = new Uri("https://fitnesstracker.com/terms")
+    });
+});
 builder.Services.AddDbContext<FitnessTrackerContext>(options =>
 {
     options.UseSqlServer(connectionString);
 });
-
+// Add local services to the container
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-var app = builder.Build();
-
 // Configure the HTTP request pipeline.
+var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+app.UseSwagger(options =>
+{
+    //options.SerializeAsV2 = true;
+});
