5addff628e2ae6894cb206ffd7ade339abe26013
Nurul Amin <NurulAmin7990@gmail.com>
2023-01-02 15:12:33 +0000

    Unit api swagger documentation added

--- FitnessTracker.WebAPI/Controllers/UnitController.cs
+++ FitnessTracker.WebAPI/Controllers/UnitController.cs
@@ -16,9 +16,33 @@ namespace FitnessTracker.WebAPI.Controllers
             this._uow = uow;
         }
 
+        /// <summary>
+        /// Get all units
+        /// </summary>
+        /// <returns>Existing units</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     GET /Unit/Get
+        ///     [{
+        ///        "id": 1,
+        ///        "unitType": "KG",
+        ///        "description": "Kilograms"
+        ///     },
+        ///     {
+        ///        "id": 2,
+        ///        "unitType": "G",
+        ///        "description": "Grams"
+        ///     }]
+        ///
+        /// </remarks>
+        /// <response code="201">Returns all the existing units</response>
+        /// <response code="204">No existing units</response>
         [HttpGet]
-        [Route("GetAll")]
-        public async Task<IActionResult> GetAllUnits()
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [Route("Get")]
+        public async Task<IActionResult> GetUnits()
         {
             IEnumerable<Unit> units = await _uow.UnitRepository.GetUnitsAsync();
 
@@ -30,6 +54,23 @@ namespace FitnessTracker.WebAPI.Controllers
             return BadRequest(units);
         }
 
+        /// <summary>
+        /// Get single unit by it's unique identifier
+        /// </summary>
+        /// <returns>Existing unit</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     GET /Unit/Get/1
+        ///     {
+        ///        "id": 1,
+        ///        "unitType": "KG",
+        ///        "description": "Kilograms"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns the existing unique unit</response>
+        /// <response code="204">No existing unit found by the given identifier</response>
         [HttpGet]
         [Route("Get/{id}")]
         public async Task<IActionResult> GetUnitById(int id)
@@ -44,6 +85,22 @@ namespace FitnessTracker.WebAPI.Controllers
             return BadRequest(unit);
         }
 
+        /// <summary>
+        /// Create new unit to store in database
+        /// </summary>
+        /// <returns>Newly created unit</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Unit/Create
+        ///     {
+        ///        "unitType": "KG",
+        ///        "description": "Kilograms"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns the new unique unit record</response>
+        /// <response code="500">Failed to create new unit record</response>
         [HttpPost]
         [Route("Create")]
         public async Task<IActionResult> AddUnit(Unit unit)
@@ -54,6 +111,18 @@ namespace FitnessTracker.WebAPI.Controllers
             return StatusCode((int)HttpStatusCode.Created);
         }
 
+        /// <summary>
+        /// Removed existing unit record from database
+        /// </summary>
+        /// <returns>Nothing</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Unit/Delete/1
+        ///
+        /// </remarks>
+        /// <response code="200">Removed the existing unit record from the database</response>
+        /// <response code="500">Failed to remove the existing unit record from the database</response>
         [HttpDelete]
         [Route("Delete/{id}")]
         public async Task<IActionResult> DeleteUnit(int id)
