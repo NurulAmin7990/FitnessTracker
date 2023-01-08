665234614d43d61826fcd0119ec60d8adaabb8db
Nurul Amin <NurulAmin7990@gmail.com>
2023-01-08 14:10:14 +0000

    Added graphql to pipeline

--- FitnessTracker.WebAPI/Program.cs
+++ FitnessTracker.WebAPI/Program.cs
@@ -2,11 +2,13 @@ using FitnessTracker.BusinessLogic.Data;
 using FitnessTracker.BusinessLogic.Interfaces;
 using FitnessTracker.BusinessLogic.Models;
 using FitnessTracker.WebAPI.Filters;
+using FitnessTracker.WebAPI.Queries;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var graphqlUri = "https://fitnesstracker.com/graphql";
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -17,7 +19,7 @@ builder.Services.AddSwaggerGen(options =>
     {
         Version = "v1",
         Title = "Fitness Tracker API",
-        Description = "An ASP.NET Core Web API for fitness tracking and workout management",
+        Description = "An ASP.NET Core Web API for fitness tracking and workout management. GraphQL: " + graphqlUri,
         TermsOfService = new Uri("https://fitnesstracker.com/terms"),
         Contact = new OpenApiContact
         {
@@ -35,11 +37,14 @@ builder.Services.AddDbContext<FitnessTrackerContext>(options =>
 builder.Services.AddCors(options => options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 // Add local services to the container
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddGraphQLServer().AddQueryType<Query>().AddProjections().AddFiltering().AddSorting();
 
 // Configure the HTTP request pipeline.
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
+    graphqlUri = "http://localhost:5023/graphql";
+
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     options.SwaggerEndpoint("/swagger/v1/swagger.json",
@@ -53,8 +58,9 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 app.MapControllers();
 app.UseCors("Open");
-app.Run();
 app.UseSwagger(options =>
 {
     //options.SerializeAsV2 = true;
 });
+app.MapGraphQL("/graphql");
+app.Run();
